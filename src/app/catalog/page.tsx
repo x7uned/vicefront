c4a48b8186a8bdb5618be1661b987cd8201f9576
Denis Goptsii <125193669+x7uned@users.noreply.github.com>
2024-08-15 19:43:48 +0300

    blyadYaZaebalsya

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -6,85 +6,40 @@ import { useTheme } from 'next-themes'
 import { Outfit } from 'next/font/google'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { Suspense, lazy, useEffect, useState } from 'react'
 import { AiOutlineHome, AiOutlineSearch } from 'react-icons/ai'
 import { MdOutlinePlaylistAdd } from 'react-icons/md'
 import Select, { SingleValue } from 'react-select'
 import PriceFilter from '../components/price.filter'
-import ProductComponent, { Product } from '../components/product.component'
+import { Product } from '../components/product.component'
 
 const outfit = Outfit({ subsets: ['latin'], weight: ['300'] })
 
+const ProductsList = lazy(() => import('../components/products.list'))
+
+interface sus {
+	value: string
+	label: string
+}
+
 const customStyles = (theme: string | undefined) => ({
-	control: (provided: any, state: any) => ({
-		...provided,
-		borderRadius: '8px',
-		backgroundColor: theme === 'dark' ? '#252525' : 'white',
-		color: theme === 'dark' ? 'white' : '#252525',
-		borderColor: state.isFocused
-			? theme === 'dark'
-				? 'white'
-				: '#dddbe0'
-			: theme === 'dark'
-			? '#45484f'
-			: '#dddbe0',
-		boxShadow: state.isFocused
-			? `0 0 0 1px ${theme === 'dark' ? '#45484f' : '#dddbe0'}`
-			: 'none',
-		transition: 'box-shadow 0.2s ease, border-color 0.2s ease',
-		borderWidth: '1px',
-		'&:hover': {
-			borderColor: theme === 'dark' ? '#333' : '#f0f0f0',
-		},
+	control: (styles: any) => ({
+		...styles,
+		backgroundColor: theme === 'dark' ? '#2d2d2d' : '#ffffff',
+		color: theme === 'dark' ? '#ffffff' : '#000000',
 	}),
-	option: (provided: any, state: any) => ({
-		...provided,
-		backgroundColor: state.isSelected
-			? theme === 'dark'
-				? '#18181a'
-				: '#dddbe0'
-			: state.isFocused
-			? theme === 'dark'
-				? '#252525'
-				: '#f0f0f0'
-			: theme === 'dark'
-			? '#252525'
-			: 'white',
-		color: theme === 'dark' ? 'white' : 'black',
-		'&:hover': {
-			backgroundColor: theme === 'dark' ? '#333' : '#f0f0f0',
-		},
+	menu: (styles: any) => ({
+		...styles,
+		backgroundColor: theme === 'dark' ? '#2d2d2d' : '#ffffff',
+		color: theme === 'dark' ? '#ffffff' : '#000000',
 	}),
-	menu: (provided: any) => ({
-		...provided,
-		borderRadius: '8px',
-		overflow: 'hidden',
-	}),
-	menuList: (provided: any) => ({
-		...provided,
-		padding: 0,
-	}),
-	placeholder: (provided: any) => ({
-		...provided,
-		color: theme === 'dark' ? '#ccc' : '#888',
-	}),
-	singleValue: (provided: any) => ({
-		...provided,
-		color: theme === 'dark' ? '#eee' : '#333',
+	singleValue: (styles: any) => ({
+		...styles,
+		color: theme === 'dark' ? '#ffffff' : '#000000',
 	}),
 })
 
-interface Brand {
-	value: string
-	label: string
-}
-
-interface Option {
-	value: string
-	label: string
-}
-
-const categoryOptions: Option[] = [
+const categoryOptions = [
 	{ value: '', label: 'All categories' },
 	{ value: 'desktop', label: 'Desktop' },
 	{ value: 'console', label: 'Console' },
@@ -92,7 +47,7 @@ const categoryOptions: Option[] = [
 	{ value: 'another', label: 'Another' },
 ]
 
-const sortOptions: Option[] = [
+const sortOptions = [
 	{ value: '', label: 'Sort' },
 	{ value: 'bestsellers', label: 'Best sellers' },
 	{ value: 'cheap', label: 'Cheap first' },
@@ -114,15 +69,15 @@ const CatalogPage: React.FC = () => {
 	const [page, setPage] = useState(1)
 	const [totalPages, setTotalPages] = useState(1)
 	const [products, setProducts] = useState<Product[]>([])
-	const [brands, setBrands] = useState<Brand[]>([])
+	const [brands, setBrands] = useState<sus[]>([])
 
 	const fetchPageData = async () => {
 		const data = { page, category, brand, sort, pricemin, pricemax }
 		const resultFindPage = await dispatch(fetchFindPage(data))
 		const resultGetBrands = await dispatch(fetchGetBrands())
 
-		const allBrandsOption: Brand = { value: '', label: 'All brands' }
-		const brandsWithAllOption = [
+		const allBrandsOption = { value: '', label: 'All brands' }
+		const brandsWithAllOption: any = [
 			allBrandsOption,
 			...(resultGetBrands.payload?.brands || []),
 		]
@@ -138,14 +93,16 @@ const CatalogPage: React.FC = () => {
 
 	const handlePageChange = (newPage: number) => {
 		setPage(newPage)
-		const newSearchParams = new URLSearchParams(searchParams)
+		const newSearchParams = new URLSearchParams(searchParams.toString())
 		newSearchParams.set('page', newPage.toString())
 		router.push(`?${newSearchParams.toString()}`)
 	}
 
-	const handleSortChange = (selectedOption: SingleValue<Option>) => {
+	const handleSortChange = (
+		selectedOption: SingleValue<{ value: string; label: string }>
+	) => {
 		setPage(1)
-		const newSearchParams = new URLSearchParams(searchParams)
+		const newSearchParams = new URLSearchParams(searchParams.toString())
 		if (selectedOption && selectedOption.value) {
 			newSearchParams.set('s', selectedOption.value)
 		} else {
@@ -154,9 +111,11 @@ const CatalogPage: React.FC = () => {
 		router.push(`?${newSearchParams.toString()}`)
 	}
 
-	const handleBrandChange = (selectedOption: SingleValue<Brand>) => {
+	const handleBrandChange = (
+		selectedOption: SingleValue<{ value: string; label: string }>
+	) => {
 		setPage(1)
-		const newSearchParams = new URLSearchParams(searchParams)
+		const newSearchParams = new URLSearchParams(searchParams.toString())
 		if (selectedOption && selectedOption.value) {
 			newSearchParams.set('b', selectedOption.value)
 		} else {
@@ -165,69 +124,17 @@ const CatalogPage: React.FC = () => {
 		router.push(`?${newSearchParams.toString()}`)
 	}
 
-	const handleCategoryChange = (selectedOption: SingleValue<Option>) => {
+	const handleCategoryChange = (
+		selectedOption: SingleValue<{ value: string; label: string }>
+	) => {
 		setPage(1)
-		const newSearchParams = new URLSearchParams(searchParams)
+		const newSearchParams = new URLSearchParams(searchParams.toString())
 		if (selectedOption && selectedOption.value) {
 			newSearchParams.set('c', selectedOption.value)
 		} else {
 			newSearchParams.delete('c')
 		}
 		router.push(`?${newSearchParams.toString()}`)
-	}
-
-	const ProductsList: React.FC = () => {
-		const PageSwitch: React.FC = () => {
-			const pageButtons = []
-			for (let i = 1; i <= totalPages; i++) {
-				pageButtons.push(
-					<button
-						key={i}
-						onClick={() => handlePageChange(i)}
-						className={`mx-1 px-3 py-1 rounded ${
-							i === page ? 'fillButton' : ''
-						}`}
-					>
-						{i}
-					</button>
-				)
-			}
-			return <div className='flex'>{pageButtons}</div>
-		}
-
-		return (
-			<>
-				<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-					{products.length !== 0 ? (
-						products.map((product: Product, index) => (
-							<div key={index} className='flex justify-center'>
-								<ProductComponent product={product} />
-							</div>
-						))
-					) : (
-						<div className='flex flex-wrap gap-[12px] justify-between'>
-							{[...Array(24)].map((_, index) => (
-								<div
-									key={index}
-									className='flex w-[240px] h-[300px] pb-8 flex-col rounded-lg border-[1px] border-[#252525] items-center animate-pulse'
-								>
-									<div className='w-full mt-4 h-[120px] bg-gray-300'></div>
-									<div className='w-full mt-4 px-2 h-8 bg-gray-300'></div>
-									<div className='flex w-full mt-10 gap-1 px-4 h-8 bg-gray-300'></div>
-								</div>
-							))}
-						</div>
-					)}
-				</div>
-				<div className='flex w-full justify-center h-48 mt-12'>
-					<div className='flex flex-col items-center w-2/3'>
-						<div className='flex mt-3'>
-							<PageSwitch />
-						</div>
-					</div>
-				</div>
-			</>
-		)
 	}
 
 	return (
@@ -262,7 +169,7 @@ const CatalogPage: React.FC = () => {
 			<div className='flex gap-2 pb-12 sm:pb-0 mt-10 w-full h-24 sm:h-10 justify-between'>
 				<div className='flex gap-2 flex-col sm:flex-row w-full'>
 					<div className='w-full'>
-						<Select<Option>
+						<Select
 							onChange={handleCategoryChange}
 							options={categoryOptions}
 							styles={customStyles(theme)}
@@ -274,8 +181,16 @@ const CatalogPage: React.FC = () => {
 				</div>
 				<div className='flex gap-2 flex-col sm:flex-row w-full'>
 					<div className='w-full'>
-						<Select<Brand>
-							onChange={handleBrandChange}
+						<Select
+							onChange={(
+								newValue:
+									| SingleValue<{ value: string; label: string }>
+									| undefined
+							) => {
+								if (newValue) {
+									console.log(newValue.value)
+								}
+							}}
 							options={brands}
 							styles={customStyles(theme)}
 							defaultValue={brands.find(b => b.value === brand)}
@@ -283,7 +198,7 @@ const CatalogPage: React.FC = () => {
 						/>
 					</div>
 					<div className='w-full'>
-						<Select<Option>
+						<Select
 							onChange={handleSortChange}
 							options={sortOptions}
 							styles={customStyles(theme)}
@@ -295,7 +210,12 @@ const CatalogPage: React.FC = () => {
 			</div>
 			<div className='mt-10 w-full'>
 				<Suspense fallback={<div>Loading products...</div>}>
-					<ProductsList />
+					<ProductsList
+						products={products}
+						totalPages={totalPages}
+						page={page}
+						handlePageChange={handlePageChange}
+					/>
 				</Suspense>
 			</div>
 		</div>
